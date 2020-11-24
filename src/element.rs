c22b7842ff7c208a483ab1f128de29858d715384
Maxim Kosterov <maxim.kosterov@gmail.com>
2020-11-24 21:14:01 +0300

    moved tag to a separate file

--- src/element.rs
+++ src/element.rs
@@ -1,49 +1,8 @@
-use crate::render::putline;
+pub mod tag;
 
 pub trait Element {
     fn size(&self) -> (u16, u16);
     fn render(&mut self);
 
     fn shouldupdate(&self) -> bool;
-}
-
-pub struct Tag {
-    width: u16,
-    text: String,
-    shouldupdate: bool,
-}
-
-impl Tag {
-    pub fn new(text: String) -> Tag {
-        let width = text.len() as u16;
-        Tag {
-            text,
-            width,
-            shouldupdate: true,
-        }
-    }
-
-    pub fn text(&self) -> &str {
-        &self.text
-    }
-
-    pub fn text_mut(&mut self) -> &mut String {
-        self.shouldupdate = true;
-        &mut self.text
-    }
-}
-
-impl Element for Tag {
-    fn size(&self) -> (u16, u16) {
-        (self.width, 1)
-    }
-
-    fn render(&mut self) {
-        self.shouldupdate = false;
-        putline(self.text.as_str());
-    }
-
-    fn shouldupdate(&self) -> bool {
-        self.shouldupdate
-    }
 }
---
+++ src/element/tag.rs
@@ -0,0 +1,43 @@
+use crate::element::Element;
+use crate::render::putline;
+
+pub struct Tag {
+    width: u16,
+    text: String,
+    shouldupdate: bool,
+}
+
+impl Tag {
+    pub fn new(text: String) -> Tag {
+        let width = text.len() as u16;
+        Tag {
+            text,
+            width,
+            shouldupdate: true,
+        }
+    }
+
+    pub fn text(&self) -> &str {
+        &self.text
+    }
+
+    pub fn text_mut(&mut self) -> &mut String {
+        self.shouldupdate = true;
+        &mut self.text
+    }
+}
+
+impl Element for Tag {
+    fn size(&self) -> (u16, u16) {
+        (self.width, 1)
+    }
+
+    fn render(&mut self) {
+        self.shouldupdate = false;
+        putline(self.text.as_str());
+    }
+
+    fn shouldupdate(&self) -> bool {
+        self.shouldupdate
+    }
+}
