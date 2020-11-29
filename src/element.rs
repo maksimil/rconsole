0953eca515d60f99197c9f1b0e42d2107c8b14d3
Maxim Kosterov <maxim.kosterov@gmail.com>
2020-11-29 19:12:02 +0300

    Added navigatioin traits

--- src/element.rs
+++ src/element.rs
@@ -3,6 +3,36 @@ pub mod tag;
 pub trait Element {
     fn size(&self) -> (u16, u16);
     fn render(&mut self);
-
     fn shouldupdate(&self) -> bool;
+}
+
+pub trait Navigate<T> {
+    fn child_ref(&self, id: usize) -> Option<&T>;
+    fn child_mut(&mut self, id: usize) -> Option<&mut T>;
+}
+
+pub trait SelfNavigate: Sized + Navigate<Self> {
+    fn navigate_ref(&self, id: &[usize]) -> Option<&Self> {
+        if id.len() == 0 {
+            Some(self)
+        } else {
+            if let Some(child) = self.child_ref(id[0]) {
+                child.navigate_ref(&id[1..])
+            } else {
+                None
+            }
+        }
+    }
+
+    fn navigate_mut(&mut self, id: &[usize]) -> Option<&mut Self> {
+        if id.len() == 0 {
+            Some(self)
+        } else {
+            if let Some(child) = self.child_mut(id[0]) {
+                child.navigate_mut(&id[1..])
+            } else {
+                None
+            }
+        }
+    }
 }
