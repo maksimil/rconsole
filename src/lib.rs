2bcdb7f813f56fafd4c794aae7324bb5b3851bee
Maxim Kosterov <maxim.kosterov@gmail.com>
2020-11-30 20:29:31 +0300

    Refactored everything (oh god...)

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,1 @@
-pub mod element;
-pub mod render;
+pub mod term;
--- src/term.rs
+++ src/term.rs
@@ -11,7 +11,7 @@ use std::io::{stdout, Write};
 const STDOUT_EXECUTION_ERR: &str = "Failed to use stdout";
 
 pub fn clear() {
-    execute!(stdout(), Clear(ClearType::All), MoveTo(0, 0));
+    execute!(stdout(), Clear(ClearType::All), MoveTo(0, 0)).expect(STDOUT_EXECUTION_ERR);
 }
 
 pub fn moveto(pos: (u16, u16)) {
@@ -27,22 +27,13 @@ pub fn imoveby(offset: (u16, u16)) {
 }
 
 pub fn putline(line: &str) {
-    execute!(stdout(), SavePosition, Print(line), RestorePosition);
+    execute!(stdout(), Print(line)).expect(STDOUT_EXECUTION_ERR);
+}
+
+pub fn putline_rp(line: &str) {
+    execute!(stdout(), SavePosition, Print(line), RestorePosition).expect(STDOUT_EXECUTION_ERR);
 }
 
 pub fn cpos() -> (u16, u16) {
-    position().expect("Was not able to get cursor positioin")
-}
-
-pub fn clear_zone((width, height): (u16, u16)) {
-    let cp = cpos();
-    let s = (0..width).fold(String::new(), |mut s, _| {
-        s.push(' ');
-        s
-    });
-    for _ in 0..height {
-        putline(s.as_str());
-        moveby((0, 1));
-    }
-    moveto(cp);
+    position().expect(STDOUT_EXECUTION_ERR)
 }
