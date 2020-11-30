1be5c837872f24b03b1029a9634e6313133714e1
Maxim Kosterov <maxim.kosterov@gmail.com>
2020-11-30 22:01:38 +0300

    Added render openings for easier buffer editing

--- src/gwin.rs
+++ src/gwin.rs
@@ -1,4 +1,6 @@
 use crate::term;
+use std::mem;
+use std::ops::Drop;
 
 #[derive(Debug, Clone)]
 pub struct GBuffer {
@@ -81,5 +83,55 @@ impl Window {
 
     pub fn edit_buffer<F: FnOnce(GBuffer) -> GBuffer>(&mut self, edit: F) {
         self.push_buffer(edit(self.buff.clone()));
+    }
+
+    pub fn open_render_edit<'s>(&'s mut self) -> Render<'s> {
+        let buffer = self.buff.clone();
+        Render {
+            window: self,
+            buffer,
+            cpos: (0, 0),
+        }
+    }
+
+    pub fn open_render_clear<'s>(&'s mut self) -> Render<'s> {
+        let bsize = self.buff.size();
+        Render {
+            window: self,
+            buffer: GBuffer::new(bsize),
+            cpos: (0, 0),
+        }
+    }
+}
+
+pub struct Render<'s> {
+    window: &'s mut Window,
+    buffer: GBuffer,
+    cpos: (usize, usize),
+}
+
+impl<'s> Render<'s> {
+    pub fn moveto(&mut self, pos: (usize, usize)) {
+        self.cpos = pos;
+    }
+
+    pub fn moveby(&mut self, (x, y): (usize, usize)) {
+        self.cpos.0 += x;
+        self.cpos.1 += y;
+    }
+
+    pub fn imoveby(&mut self, (x, y): (usize, usize)) {
+        self.cpos.0 -= x;
+        self.cpos.1 -= y;
+    }
+    pub fn putline(&mut self, s: &str) {
+        self.buffer.putline(self.cpos, s);
+    }
+}
+
+impl<'s> Drop for Render<'s> {
+    fn drop(&mut self) {
+        self.window
+            .push_buffer(mem::replace(&mut self.buffer, GBuffer::new((0, 0))));
     }
 }
