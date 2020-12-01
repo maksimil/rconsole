dbe0a114c7bf9b2312bd1c04c482d55af90baeb0
Maxim Kosterov <maxim.kosterov@gmail.com>
2020-12-01 20:06:29 +0300

    rewrote buffer to save in line form (window does not work now)

--- src/gwin/buffer.rs
+++ src/gwin/buffer.rs
@@ -1,25 +1,60 @@
-#[derive(Debug, Clone)]
+#[derive(Debug)]
+pub struct PutLine(pub usize, pub String);
+
+impl PutLine {
+    fn sx(&self) -> usize {
+        self.0
+    }
+
+    fn len(&self) -> usize {
+        self.1.len()
+    }
+
+    fn ex(&self) -> usize {
+        self.sx() + self.len()
+    }
+}
+
+#[derive(Debug)]
 pub struct GBuffer {
-    buff: Vec<String>,
+    lines: Vec<Vec<PutLine>>,
 }
 
 impl GBuffer {
-    pub fn new((width, height): (usize, usize)) -> GBuffer {
-        let s = (0..width).map(|_| " ").collect::<String>();
+    pub fn new(height: usize) -> GBuffer {
         GBuffer {
-            buff: (0..height).map(|_| s.clone()).collect::<Vec<String>>(),
+            lines: Vec::with_capacity(height),
         }
     }
 
-    pub fn size(&self) -> (usize, usize) {
-        (self.buff[0].len(), self.buff.len())
+    pub fn putline_str(&mut self, (i, j): (usize, usize), s: &str) {
+        self.putline((i, j), String::from(s));
     }
 
-    pub fn putline(&mut self, (i, j): (usize, usize), s: &str) {
-        self.buff[j].replace_range(i..i + s.len(), s)
+    pub fn putline(&mut self, (x, y): (usize, usize), s: String) {
+        self.expand(y + 1);
+
+        let line = &mut self.lines[y];
+        let pl = PutLine(x, s);
+
+        if line.len() > 0 && line[line.len() - 1].ex() > pl.sx() {
+            panic!("Function putline does not allow overlapping line putting");
+        } else {
+            line.push(pl);
+        }
     }
 
-    pub fn buffer(&self) -> &Vec<String> {
-        &self.buff
+    pub fn expand(&mut self, height: usize) {
+        while self.lines.len() < height {
+            self.lines.push(Vec::new());
+        }
+    }
+
+    pub fn height(&self) -> usize {
+        self.lines.len()
+    }
+
+    pub fn lines(&self) -> &Vec<Vec<PutLine>> {
+        &self.lines
     }
 }
