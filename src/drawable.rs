a3fe24defa8ea1f3ad45f982b271391acfc0dc3a
Maxim Kosterov <maxim.kosterov@gmail.com>
2020-12-04 19:27:08 +0300

    Added StdDrawable enum

--- src/drawable.rs
+++ src/drawable.rs
@@ -7,3 +7,24 @@ pub trait Drawable {
     fn size(&self) -> (usize, usize);
     fn draw(&self, r: &mut Render);
 }
+
+pub enum StdDrawable<'s> {
+    Tag(tag::TagStr<'s>),
+    List(list::List<StdDrawable<'s>>),
+}
+
+impl<'s> Drawable for StdDrawable<'s> {
+    fn size(&self) -> (usize, usize) {
+        match self {
+            Self::Tag(s) => s.size(),
+            Self::List(s) => s.size(),
+        }
+    }
+
+    fn draw(&self, r: &mut Render) {
+        match self {
+            Self::Tag(s) => s.draw(r),
+            Self::List(s) => s.draw(r),
+        }
+    }
+}
